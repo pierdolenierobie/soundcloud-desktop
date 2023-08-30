de37606085e709f92aeb8e4165c45a5b63391777
vaul <zgredulson@gmail.com>
2023-08-30 19:11:44 +0200

    Update window.py

--- src/window.py
+++ src/window.py
@@ -10,25 +10,25 @@ class win:
         while True:
             window.evaluate_js('''
 
-                var playControl =   document.querySelector(".playControl");
-                var is_paused = playControl.className.includes('playing') ? false : true
+                var playControl     = document.querySelector(".playControl");
+                var is_paused       = playControl.className.includes('playing') ? false : true
 
-                var img =           document.querySelector(".sc-artwork-4x").style.backgroundImage.slice(4, -1).replace(/"/g, "").replaceAll("50", "500");
+                var img             = document.querySelector(".sc-artwork-4x").style.backgroundImage.slice(4, -1).replace(/"/g, "").replaceAll("50", "500");
 
-                var author =        document.querySelector(".playbackSoundBadge__lightLink").innerHTML;
+                var author          = document.querySelector(".playbackSoundBadge__lightLink").innerHTML;
 
-                var song_url =     document.querySelector(".playbackSoundBadge__titleLink").href;
-                var title =         document.querySelector(".playbackSoundBadge__titleLink > span:nth-child(2)").innerHTML;
+                var song_url        = document.querySelector(".playbackSoundBadge__titleLink").href;
+                var title           = document.querySelector(".playbackSoundBadge__titleLink > span:nth-child(2)").innerHTML;
 
-                var curr_time =     document.querySelector(".playbackTimeline__timePassed > span:nth-child(2)").innerHTML.split(":");
-                var ttal_time =     document.querySelector(".playbackTimeline__duration > span:nth-child(2)").innerHTML.split(":");
+                var curr_time       = document.querySelector(".playbackTimeline__timePassed > span:nth-child(2)").innerHTML.split(":");
+                var ttal_time       = document.querySelector(".playbackTimeline__duration > span:nth-child(2)").innerHTML.split(":");
 
                 var data = {
-                    is_paused: is_paused,
-                    song_url: song_url,
-                    author: author,
-                    title: title, 
-                    img: img, 
+                    is_paused:  is_paused,
+                    song_url:   song_url,
+                    author:     author,
+                    title:      title, 
+                    img:        img, 
                     curr_time: {
                         minutes: curr_time[0],
                         seconds: curr_time[1]
