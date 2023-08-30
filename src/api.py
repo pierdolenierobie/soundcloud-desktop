1acd844c8252ce44e69911dcae4f7a8abed9a4f5
vaul <zgredulson@gmail.com>
2023-08-30 19:30:43 +0200

    Update api.py

--- src/api.py
+++ src/api.py
@@ -12,12 +12,12 @@ class api:
             "author":       None,
             "song_url":     None,
             "curr_time":    {
-                "minutes": None,
-                "seconds": None
+                "minutes":  None,
+                "seconds":  None
             },
             "ttal_time":    {
-                "minutes": None,
-                "seconds": None
+                "minutes":  None,
+                "seconds":  None
             }
         }
 
@@ -26,23 +26,23 @@ class api:
         self._rpc.run()
 
     def set_song_data(self, data):
-        # i dont know what im even doing at this point alr??
+        # i dont know what im even doing at this point alr?
         
-        self.song_data["is_paused"] =           data["is_paused"]
+        self.song_data["is_paused"] = data["is_paused"]
 
         self.song_data["ttal_time"]['minutes'] = int(data['ttal_time']['minutes']) - int(data['curr_time']['minutes'])
         self.song_data["ttal_time"]['seconds'] = int(data['ttal_time']['seconds']) - int(data['curr_time']['seconds'])
-    
         
-        if self.song_data['author'] is None or self.song_data['author'] != data['author']:
+        
+        if self.song_data['title'] is None or self.song_data['title'] != data['title']:
             
-            self.song_data["img"] =                 data["img"]
 
-            self.song_data["title"] =               data["title"]
-            self.song_data["author"] =              data["author"]
-            self.song_data["song_url"] =              data["song_url"]
+            self.song_data["img"]                   = data["img"]
+            self.song_data["title"]                 = data["title"]
+            self.song_data["author"]                = data["author"]
+            self.song_data["song_url"]              = data["song_url"]
             
-            self.song_data["curr_time"]['minutes'] = int(data['curr_time']['minutes']) 
-            self.song_data["curr_time"]['seconds'] = int(data['curr_time']['seconds'])          
+            self.song_data["curr_time"]['minutes']  = int(data['curr_time']['minutes']) 
+            self.song_data["curr_time"]['seconds']  = int(data['curr_time']['seconds'])          
             
         self._rpc.update(self.song_data)
